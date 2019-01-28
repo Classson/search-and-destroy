583378f7b6f4f0805ccc37a38d2d317b2d462c59
Torri Raines <raines.torri@gmail.com>
2019-01-28 15:03:31 -0500

    all done woop time for extra credit

--- min-jumps/min-jumps.js
+++ min-jumps/min-jumps.js
@@ -1,18 +1,21 @@
 'use strict';
 
-let counter = 0;
 // Complete this algo
 function minJumps (arr){
+  if (arr.length === 0){
+    return 1
+  }
+  let counter = 0;
   let current = arr[0];
-  if(arr[current] > arr.length){
-    counter++;
-    return counter
+  if (current >= (arr.length) - 1){
+    return 1;
   } else {
-    let subArr = arr.slice(0, arr[current])
+    let subArr = arr.slice(1, current + 1)
     current = arr.indexOf(Math.max(...subArr))
     counter++;
-    return minJumps(arr.splice(0, current))
+    arr.splice(0, current)
+    return counter + minJumps(arr)
   }
-};
+}
 
 module.exports = minJumps
